531bee60a7399183abf06b548d0731dc81d0859d
nikosT <ntriantafyl@cslab.ece.ntua.gr>
2025-09-25 18:31:41 +0300

    Fix in run_tuning

--- slurm/openmp_examples/run_tuning.sh
+++ slurm/openmp_examples/run_tuning.sh
@@ -4,12 +4,8 @@
 #SBATCH --error=my_tuning_%j.err   # Error file name (%j expands to jobID)
 #SBATCH --partition=normal          # Partition name
 #SBATCH --nodes=1                    # Number of nodes
-#SBATCH --ntasks=1                    # Number of tasks
-#SBATCH --ntasks-per-node=1          # Number of tasks per node
-#SBATCH --cpus-per-task=4          # Number of tasks per node
+#SBATCH --ntasks=4                    # Number of tasks
+#SBATCH --cpus-per-task=1          # Number of tasks per node
 #SBATCH --time=00:01:00              # Time limit (HH:MM:SS)
-
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
-
 
 $HOME/openmp_examples/ScalabilityTuningRF.py
